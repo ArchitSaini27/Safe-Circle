47efe9bfa79417ce462b113cded97a37915691d1
Archit Saini <archit.as.802@gmail.com>
2023-09-25 21:12:12 +0530

    Initial Commit

---
+++ app/src/main/java/com/archit/safecircle/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.archit.safecircle
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_activity)
+
+        val bottomNavigation: BottomNavigationView=findViewById(R.id.bottom_bar_navigation)
+        bottomNavigation.setOnItemSelectedListener {
+
+            when (it.itemId) {
+                R.id.navbar_guard -> {
+                    inflateFragment(GuardScreenFragment.newInstance())
+                }
+                R.id.navbar_home -> {
+                    inflateFragment(HomeScreenFragment.newInstance())
+                }
+                R.id.navbar_dashboard -> {
+                    inflateFragment(DashboardScreenFragment.newInstance())
+                }
+                else -> {
+                    inflateFragment(ProfileScreenFragment.newInstance())
+                }
+            }
+            true
+        }
+        bottomNavigation.selectedItemId=(R.id.navbar_home)
+
+    }
+
+    private fun inflateFragment(fragment: Fragment) {
+        val transaction=supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.frame_layout,fragment)
+        transaction.commit()
+
+    }
+}
